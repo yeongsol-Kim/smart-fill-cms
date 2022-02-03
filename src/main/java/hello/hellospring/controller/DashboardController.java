03e1fe8cbd7b94400ec4631d24c900a524016503
sinminseok12 <sin1768@naver.com>
2022-02-03 22:19:35 +0900

    commot

---
+++ src/main/java/hello/hellospring/controller/DashboardController.java
@@ -0,0 +1,18 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.domain.FillLog;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class DashboardController {
+
+
+    @GetMapping("/dashboard1")
+    public String dashboard(Model model) {
+        return "dashboard/register_place";
+    }
+}
