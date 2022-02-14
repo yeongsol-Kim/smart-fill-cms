a4998fbbbc3c7a0affae7d011db1348c4f5f69cc
onagu <onagu7167@gmail.com>
2022-02-14 13:31:07 +0900

    220213 - packageㅂmodule

--- src/main/java/hello/rest/repository/AuthorityRepository.java
+++
@@ -1,7 +0,0 @@
-package hello.rest.repository;
-
-import hello.rest.entity.Authority;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorityRepository extends JpaRepository<Authority, String> {
-}
