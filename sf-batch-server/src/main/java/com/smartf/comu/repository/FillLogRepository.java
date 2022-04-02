58d7f66e76929f120eb5a3b8e79a5973b2d731c5
onagu <onagu7167@gmail.com>
2022-04-02 21:53:26 +0900

    2200402 some fix and new db

--- sf-batch-server/src/main/java/com/smartf/comu/repository/FillLogRepository.java
+++ sf-batch-server/src/main/java/com/smartf/comu/repository/FillLogRepository.java
@@ -2,6 +2,7 @@ package com.smartf.comu.repository;
 
 import com.smartf.comu.domain.Log;
 import com.smartf.comu.dto.LogReportDto;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -23,9 +24,18 @@ public interface FillLogRepository extends JpaRepository<Log, Long> {
             "SELECT " +
                     "new com.smartf.comu.dto.LogReportDto(MONTH(l.datetime) as month, SUM(l.amount) as sumAmount) " +
                     "FROM Log l " +
+                    "WHERE l.branchId = :id " +
+                    "GROUP BY MONTH(l.datetime)"
+    )
+    List<LogReportDto> findByBranchIdGroupByMonthWithJPQL(Long id);
+
+    @Query(value =
+            "SELECT " +
+                    "new com.smartf.comu.dto.LogReportDto(MONTH(l.datetime) as month, SUM(l.amount) as sumAmount) " +
+                    "FROM Log l " +
                     "WHERE l.carNumber = :id " +
                     "GROUP BY MONTH(l.datetime)"
     )
-    List<LogReportDto> findByIdGroupByMonthWithJPQL(String id);
+    List<LogReportDto> findByCarNumberGroupByMonthWithJPQL(String id);
 
 }
--- sf-batch-server/src/main/java/com/smartf/comu/service/FillLogService.java
+++ sf-batch-server/src/main/java/com/smartf/comu/service/FillLogService.java
@@ -4,6 +4,7 @@ import com.smartf.comu.domain.Log;
 import com.smartf.comu.dto.LogReportDto;
 import com.smartf.comu.repository.FillLogRepository;
 import com.smartf.comu.util.SecurityUtil;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -30,20 +31,25 @@ public class FillLogService {
 
     public List<Log> getLogsByBranchId(Long branchId) {
         return fillLogRepository.findByBranchId(branchId);
+
+        // Sort
+        //return fillLogRepository.findByBranchId(branchId, Sort.by(Sort.Direction.DESC, "datetime"));
     }
 
     public List<Log> getMyBranchLogs() {
         Long id = SecurityUtil.getCurrentDependentId().orElse(null);
         return fillLogRepository.findByBranchId(id);
+        //return fillLogRepository.findByBranchId(id, Sort.by(Sort.Direction.DESC, "datetime"));
     }
 
     public List<LogReportDto> getMyGraphData() {
-        List<LogReportDto> logReportDto = fillLogRepository.findGroupByMonthWithJPQL();
+
+        List<LogReportDto> logReportDto = fillLogRepository.findByBranchIdGroupByMonthWithJPQL(SecurityUtil.getCurrentDependentId().orElse(null));
         return logReportDto;
     }
 
     public List<LogReportDto> getMyGraphDataByCarNumber(String carNumber) {
-        List<LogReportDto> logReportDto = fillLogRepository.findByIdGroupByMonthWithJPQL(carNumber);
+        List<LogReportDto> logReportDto = fillLogRepository.findByCarNumberGroupByMonthWithJPQL(carNumber);
         return logReportDto;
     }
 }
